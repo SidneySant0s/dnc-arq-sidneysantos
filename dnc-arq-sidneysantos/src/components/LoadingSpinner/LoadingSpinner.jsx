c9986957528f7b14783ab16e84592e87eda9456c
Sidney Santos <sidney.santos.zx@gmail.com>
2025-08-05 22:23:55 -0300

    first commit

---
+++ dnc-arq-sidneysantos/src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -0,0 +1,15 @@
+import './LoadingSpinner.css'
+import LoadingSpinnerGif from '../../assets/loading.gif'
+
+
+
+function LoadingSpinner(){
+    return(
+        <div className="d-flex al-center jc-center loading-overlay-container">
+                <img src={LoadingSpinnerGif} alt="Loading" height="80px" />
+            
+        </div>
+    )
+}
+
+export default LoadingSpinner
